782f45de04c33264ac8f92916454e8e6ac49cef8
André Bastos <andre.bastos.lei@gmail.com>
2020-03-18 17:16:27 +0000

    Updates

---
+++ precache-manifest.d108c77c4d06d5b8bbc28c8e96c589eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef45aaa5e34353db45b957417a7f3e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "001d52eedcfcffb5da14",
+    "url": "/static/css/main.314cad6b.chunk.css"
+  },
+  {
+    "revision": "b8000fa524fd4714b181",
+    "url": "/static/js/2.64bbaf34.chunk.js"
+  },
+  {
+    "revision": "7f61c796f350fdf2a798e950bba81e8d",
+    "url": "/static/js/2.64bbaf34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "001d52eedcfcffb5da14",
+    "url": "/static/js/main.f2717600.chunk.js"
+  },
+  {
+    "revision": "231617520aa312a61fb8",
+    "url": "/static/js/runtime-main.d080a0eb.js"
+  }
+]);
