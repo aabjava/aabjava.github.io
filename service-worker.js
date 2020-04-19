ffd4ac98b6aa04d8fc67e8ab6f984d7a3d5c51e4
André Bastos <andre.bastos.lei@gmail.com>
2020-04-19 17:07:30 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3710be759366210577855d923665ea6a.js"
+  "/precache-manifest.2779dbadf701becd4c39bada61402970.js"
 );
 
 self.addEventListener('message', (event) => {
